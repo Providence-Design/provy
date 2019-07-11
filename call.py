61a5fbd9e11ea5a738a28f42cff8a5d1f5a92c49
Providence-Design <52656146+Providence-Design@users.noreply.github.com>
2019-07-11 12:05:09 +0000

    Add files via upload

---
+++ call.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+def get_username(): 
+    username = str((input("Please enter username: ")))
+    return username
+print (get_username())
+
+def get_age():
+    age = int((input('please enter age:')))
+    return age
+print (get_age())
+ 
